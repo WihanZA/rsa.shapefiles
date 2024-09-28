8ebc14a5c1d63cc180c6c845df4f4a4fe1f00d92
WihanZA <wihanmarais@sun.ac.za>
2024-09-28 19:51:20 +0200

    Initial commit

---
+++ code/setup.R
@@ -0,0 +1,44 @@
+library(usethis)
+library(devtools)
+
+# usethis::use_git()
+# usethis::use_github()
+# usethis::use_tidy_github()
+# usethis::use_tidy_github_actions()
+# usethis::use_tidy_github_labels()
+# usethis::use_pkgdown_github_pages()
+
+# proj_sitrep()
+# git_sitrep()
+
+# Set up a new package directory
+# create_package("path/to/your/package")
+
+# # Navigate to the new package directory
+# setwd("path/to/your/package")
+
+# # Initialize Git
+# use_git()
+
+# # Create common files
+# use_readme_rmd()
+# use_mit_license("Your Name")
+# use_news_md()
+# use_code_of_conduct()
+# use_lifecycle_badge("Experimental")
+
+# # Set up testing infrastructure
+# use_testthat()
+
+# # Add recommended dependencies
+# use_package("dplyr")
+# use_package("ggplot2")
+
+# # Create an R script for functions
+# use_r("my_functions")
+
+# # Document the package
+# document()
+
+# # Build the package
+# build()
